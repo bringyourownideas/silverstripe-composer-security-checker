ca24b122fdf7224c961591229968fce843772dc1
Peter Thaleikis <petert@catalyst.net.nz>
2015-08-23 23:54:27 +1200

    adding queuedjob to run the task

---
+++ code/jobs/ComposerSecurityCheckJob.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Composer security checker job. Runs the task which does the check as a queuedjob.
+ *
+ * @author Peter Thaleikis
+ * @license MIT
+ */
+class ComposerSecurityCheckJob extends AbstractQueuedJob implements QueuedJob {
+	/**
+	 * The task to run
+	 *
+	 * @var BuildTask
+	 */
+	protected $task;
+
+	/**
+	 * define the title
+	 *
+	 * @return string
+	 */
+	public function getTitle() {
+		return _t(
+			'ComposerSecurity.Title',
+			'Check if composer dependencies have known vulnerabilities.'
+		);
+	}
+
+	/**
+	 * define the type.
+	 */
+	public function getJobType() {
+		$this->totalSteps = 1;
+
+		return QueuedJob::QUEUED;
+	}
+
+	/**
+	 * init
+	 */
+	public function setup() {
+		// create the instance of the task
+		$this->task = new SecurityCheckerTask();
+	}
+
+	/**
+	 * processes the task as a job
+	 */
+	public function process() {
+		// run the task
+		$this->task->run(new SS_HTTPRequest());
+
+		// mark job as completed
+		$this->isComplete = true;
+	}
+}
