202c0962b6d5817dc7a7465a0ce336362238fc45
Peter Thaleikis <petert@catalyst.net.nz>
2015-09-07 14:39:08 +1200

    Increasing readiblity by adding tabs

--- code/tasks/SecurityCheckerTask.php
+++ code/tasks/SecurityCheckerTask.php
@@ -42,11 +42,11 @@ class SecurityCheckerTask extends BuildTask {
 					// Is this vulnerability known? No, lets add it.
 					if ((int) $vulnerability->count() === 0) {
 						$vulnerability = new ComposerSecurityVulnerability();
-						$vulnerability->Package	= $package;
-						$vulnerability->Version	= $packageDetails['version'];
-						$vulnerability->Title	= $details['title'];
-						$vulnerability->ExternalLink = $details['link'];
-						$vulnerability->CVE		= $details['cve'];
+						$vulnerability->Package			= $package;
+						$vulnerability->Version			= $packageDetails['version'];
+						$vulnerability->Title			= $details['title'];
+						$vulnerability->ExternalLink	= $details['link'];
+						$vulnerability->CVE				= $details['cve'];
 						$vulnerability->write();
 
 						// add the new entries to the list of the remaining entries
